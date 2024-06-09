e5619f2b464536c294db88115e6195c8bcc16241
PFrek <pedrogayaf@gmail.com>
2024-06-09 15:28:04 -0300

    Add fetching and parsing of xml from rss feeds

---
+++ internal/scrapper/scraper_test.go
@@ -0,0 +1,89 @@
+package scrapper
+
+import (
+	"strings"
+	"testing"
+)
+
+// TESTS
+
+func TestParseXMLOneEntry(t *testing.T) {
+	xml := `<rss>
+<channel>
+	<item>
+		<title>1st entry</title>
+		<link>www.entry-1.com</link>
+		<pubDate>Wed, 05 Jun 2024 00:00:00 +0000</pubDate>
+	</item>
+</channel>
+</rss>`
+
+	reader := strings.NewReader(xml)
+
+	feedData, err := parseXML(reader)
+	if err != nil {
+		t.Fatalf("Failed: %v", err)
+	}
+
+	if len(feedData.Items) != 1 {
+		t.Fatalf("Invalid number of items expected 1 got %d", len(feedData.Items))
+	}
+
+	if feedData.Items[0].Title != "1st entry" {
+		t.Fatalf("Invalid title: expected '1st entry' got '%s'", feedData.Items[0].Title)
+	}
+}
+
+func TestParseXMLManyEntries(t *testing.T) {
+	xml := `<rss>
+<channel>
+	<item>
+		<title>1st entry</title>
+		<link>www.entry-1.com</link>
+		<pubDate>Wed, 01 Jun 2024 00:00:00 +0000</pubDate>
+	</item>
+	<item>
+		<title>2nd entry</title>
+		<link>www.entry-2.com</link>
+		<pubDate>Wed, 02 Jun 2024 00:00:00 +0000</pubDate>
+	</item>
+	<item>
+		<title>3rd entry</title>
+		<link>www.entry-3.com</link>
+		<pubDate>Wed, 04 Jun 2024 00:00:00 +0000</pubDate>
+	</item>
+</channel>
+</rss>`
+
+	reader := strings.NewReader(xml)
+
+	feedData, err := parseXML(reader)
+	if err != nil {
+		t.Fatalf("Failed: %v", err)
+	}
+
+	if len(feedData.Items) != 3 {
+		t.Fatalf("Invalid number of items expected 3 got %d", len(feedData.Items))
+	}
+
+	if feedData.Items[0].Title != "1st entry" {
+		t.Fatalf("Invalid title: expected '1st entry' got '%s'", feedData.Items[0].Title)
+	}
+
+	if feedData.Items[1].Title != "2nd entry" {
+		t.Fatalf("Invalid title: expected '2nd entry' got '%s'", feedData.Items[0].Title)
+	}
+
+	if feedData.Items[2].Title != "3rd entry" {
+		t.Fatalf("Invalid title: expected '3rd entry' got '%s'", feedData.Items[0].Title)
+	}
+}
+
+func TestFetchFeedData(t *testing.T) {
+	url := "https://blog.boot.dev/index.xml"
+
+	_, err := fetchDataFromFeed(url)
+	if err != nil {
+		t.Fatalf("Failed: %v", err)
+	}
+}
---
+++ internal/scrapper/scrapper.go
@@ -0,0 +1,44 @@
+package scrapper
+
+import (
+	"encoding/xml"
+	"fmt"
+	"io"
+	"net/http"
+)
+
+type FeedItem struct {
+	XMLName xml.Name `xml:"item"`
+	Title   string   `xml:"title"`
+	Link    string   `xml:"link"`
+	PubDate string   `xml:"pubDate"`
+}
+
+type FeedData struct {
+	XMLName xml.Name   `xml:"rss"`
+	Items   []FeedItem `xml:"channel>item"`
+}
+
+func fetchDataFromFeed(feedUrl string) (FeedData, error) {
+	resp, err := http.Get(feedUrl)
+	if err != nil {
+		return FeedData{}, fmt.Errorf("Failed to fetch RSS feeed: %v", err)
+	}
+	defer resp.Body.Close()
+
+	feedData, err := parseXML(resp.Body)
+
+	return feedData, nil
+}
+
+func parseXML(xmlData io.Reader) (FeedData, error) {
+	var feedData FeedData
+
+	decoder := xml.NewDecoder(xmlData)
+	err := decoder.Decode(&feedData)
+	if err != nil {
+		return FeedData{}, err
+	}
+
+	return feedData, nil
+}
